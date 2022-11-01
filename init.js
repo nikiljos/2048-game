5965cb3c69a61aaa5efadd5e7cc35331ecfd77cd
Nikhil Jose <nikhiljose12345@gmail.com>
2022-11-01 21:20:48 +0530

    update game logic, test scripts, update prod workflow

---
+++ init.js
@@ -0,0 +1,5 @@
+if(testing){
+    let testScript=document.createElement("script");
+    testScript.setAttribute("src","test.js");
+    document.body.appendChild(testScript)
+}
---
+++ test.js
@@ -0,0 +1,39 @@
+let testInterval;
+let resHistory = "";
+function gameEndTest() {
+    testInterval = setInterval(() => {
+        let options = ["Down", "Up", "Right", "Left"];
+        let key = options[Math.floor(Math.random() * options.length)];
+        console.log("Clicking", key);
+        document.dispatchEvent(
+            new KeyboardEvent("keyup", { key: `Arrow${key}` })
+        );
+    }, 100);
+}
+
+function stopTesting() {
+    clearInterval(testInterval);
+}
+
+function updateHistory(fRes) {
+    console.log(fRes);
+    hCombi.forEach((elt) => {
+        elt.forEach((pos) => {
+            let val = fRes[pos];
+            if (val == undefined) {
+                val = "-";
+            }
+            resHistory += `${val},`;
+        });
+        resHistory += "\n";
+    });
+    resHistory += "\n";
+}
+
+function downloadHistory() {
+    var link = document.createElement("a");
+    link.download = "result.csv";
+    var blob = new Blob([resHistory], { type: "text/csv" });
+    link.href = window.URL.createObjectURL(blob);
+    link.click();
+}
